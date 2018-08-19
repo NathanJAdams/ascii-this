098c29a42ff3b4a3819e2f00a303e8c457afa280
Nathan <n8style@yahoo.com>
2018-08-19 12:28:24 +0100

    Added secret retriever

--- users/main/java/users/db/DatabaseReferenceCreator.java
+++ users/main/java/users/db/DatabaseReferenceCreator.java
@@ -6,11 +6,11 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import org.apache.commons.io.input.ReaderInputStream;
+import users.security.SecretRetriever;
 
-import java.io.CharArrayReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 
 public class DatabaseReferenceCreator {
@@ -22,8 +22,11 @@ public class DatabaseReferenceCreator {
     }
 
     private static DatabaseReference createDatabaseConnection() {
-        StringBuilder sb = createServiceAccountStringBuilder();
-        InputStream serviceAccount = createServiceAccountInputStream(sb);
+        InputStream serviceAccount = createServiceAccountInputStream();
+        if (serviceAccount == null) {
+            System.out.println("Null service account");
+            return null;
+        }
         try {
             GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
             FirebaseOptions options = new FirebaseOptions.Builder()
@@ -38,48 +41,12 @@ public class DatabaseReferenceCreator {
         return FirebaseDatabase.getInstance().getReference();
     }
 
-    private static InputStream createServiceAccountInputStream(StringBuilder sb) {
-        char[] chars = new char[sb.length()];
-        sb.getChars(0, sb.length(), chars, 0);
-        Reader reader = new CharArrayReader(chars);
-        return new ReaderInputStream(reader, StandardCharsets.UTF_8);
-    }
-
-    private static StringBuilder createServiceAccountStringBuilder() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{");
-        appendKeyValue(sb, "type", "service_account");
-        appendKeyEnvVar(sb, "project_id");
-        appendKeyEnvVar(sb, "private_key_id");
-        appendKeyEnvVar(sb, "private_key");
-        appendKeyEnvVar(sb, "client_email");
-        appendKeyEnvVar(sb, "client_id");
-        appendKeyEnvVar(sb, "auth_uri");
-        appendKeyEnvVar(sb, "token_uri");
-        appendKeyEnvVar(sb, "auth_provider_x509_cert_url");
-        appendKeyEnvVar(sb, "client_x509_cert_url");
-        sb.setLength(sb.length() - 1);
-        sb.append("}");
-        return sb;
-    }
-
-    private static void appendKeyEnvVar(StringBuilder sb, String key) {
-        appendKeyEnvVar(sb, key, key);
-    }
-
-    private static void appendKeyEnvVar(StringBuilder sb, String key, String envVarKey) {
-        String envVar = System.getenv(envVarKey);
-        appendKeyValue(sb, key, envVar);
-    }
-
-    private static void appendKeyValue(StringBuilder sb, String key, String value) {
-        sb.append('"');
-        sb.append(key);
-        sb.append('"');
-        sb.append(':');
-        sb.append('"');
-        sb.append(value);
-        sb.append('"');
-        sb.append(',');
+    private static InputStream createServiceAccountInputStream() {
+        String secret = SecretRetriever.getSecretAsString("prod/firebase/serviceaccount");
+        if (secret == null) {
+            return null;
+        }
+        StringReader sr = new StringReader(secret);
+        return new ReaderInputStream(sr, StandardCharsets.UTF_8);
     }
 }
--- users/main/java/users/db/KeyConverter.java
+++ users/main/java/users/db/KeyConverter.java
@@ -1,58 +1,51 @@
 package users.db;
 
+import it.unimi.dsi.fastutil.chars.Char2CharMap;
+import it.unimi.dsi.fastutil.chars.Char2CharOpenHashMap;
+
 public class KeyConverter {
+    private static final Char2CharMap ENCODED = new Char2CharOpenHashMap();
+    private static final Char2CharMap DECODED = new Char2CharOpenHashMap();
+
+    static {
+        addMapping('.', 'P');
+        addMapping('$', 'D');
+        addMapping('#', 'H');
+        addMapping('[', 'O');
+        addMapping(']', 'C');
+        addMapping('/', 'S');
+        addMapping('_', 'U');
+    }
+
+    private static void addMapping(char raw, char encoded) {
+        ENCODED.put(raw, encoded);
+        DECODED.put(encoded, raw);
+    }
+
     public static String toKey(String raw) {
         StringBuilder sb = new StringBuilder();
-        for (char c : raw.toCharArray()) {
-            if (c == '.') {
-                sb.append("_P");
-            } else if (c == '$') {
-                sb.append("_D");
-            } else if (c == '#') {
-                sb.append("_H");
-            } else if (c == '[') {
-                sb.append("_O");
-            } else if (c == ']') {
-                sb.append("_C");
-            } else if (c == '/') {
-                sb.append("_S");
-            } else if (c == '_') {
-                sb.append("__");
-            } else {
-                sb.append(c);
+        for (char encoded : raw.toCharArray()) {
+            char replacement = ENCODED.getOrDefault(encoded, encoded);
+            if (encoded != replacement) {
+                sb.append('_');
             }
+            sb.append(replacement);
         }
         return sb.toString();
     }
 
     public static String toRaw(String key) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < key.length() - 1; i++) {
-            char c = key.charAt(i);
-            if (c == '_') {
-                i++;
-                c = key.charAt(i);
-                if (c == 'P') {
-                    sb.append('.');
-                } else if (c == 'D') {
-                    sb.append('$');
-                } else if (c == 'H') {
-                    sb.append('#');
-                } else if (c == 'O') {
-                    sb.append('[');
-                } else if (c == 'C') {
-                    sb.append(']');
-                } else if (c == 'S') {
-                    sb.append('/');
-                } else {
-                    // ignore bad encoding
-                    sb.append(c);
-                }
-            } else {
-                sb.append(c);
-            }
-        }
         int lastIndex = key.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < lastIndex; i++) {
+            char decoded = key.charAt(i);
+            if (decoded == '_') {
+                i++;
+                decoded = key.charAt(i);
+                decoded = DECODED.getOrDefault(decoded, decoded);
+            }
+            sb.append(decoded);
+        }
         char lastChar = key.charAt(lastIndex);
         // ignore bad encoding
         sb.append(lastChar);
