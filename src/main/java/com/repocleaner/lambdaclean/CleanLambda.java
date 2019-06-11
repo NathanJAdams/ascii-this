0ceda7a2c469d57e9401069eda67784b0849517d
Nathan <n8style@yahoo.com>
2019-06-11 22:34:36 +0100

    bug fix

--- src/main/java/com/repocleaner/lambdaclean/CleanLambda.java
+++ src/main/java/com/repocleaner/lambdaclean/CleanLambda.java
@@ -4,15 +4,19 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.repocleaner.cleaner.Cleaner;
+import com.repocleaner.core.initiators.InitiatorGsonCustomiser;
 import com.repocleaner.core.util.RepoCleanerException;
 import com.repocleaner.core.util.json.JsonUtil;
 import com.repocleaner.s3.S3CleanIO;
+import com.repocleaner.sink.SinkGsonCustomiser;
+import com.repocleaner.source.SourceGsonCustomiser;
 
 public class CleanLambda implements RequestHandler<S3Event, Void> {
     private static final Cleaner CLEANER = new Cleaner();
 
     public Void handleRequest(S3Event event, Context context) {
-        try (S3CleanIO cleanIO = new S3CleanIO(event, new JsonUtil())) {
+        JsonUtil jsonUtil = new JsonUtil(new InitiatorGsonCustomiser(), new SinkGsonCustomiser(), new SourceGsonCustomiser());
+        try (S3CleanIO cleanIO = new S3CleanIO(event, jsonUtil)) {
             System.out.println("Start cleaning");
             CLEANER.clean(cleanIO);
             System.out.println("Finished cleaning");
