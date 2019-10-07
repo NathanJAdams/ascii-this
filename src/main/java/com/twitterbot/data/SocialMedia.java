87b6a0a50d63fe7551c1c02ba3d3bcdea0d34f09
Nathan <n8style@yahoo.com>
2019-10-07 21:13:03 +0100

    Multiple content count patterns

--- src/main/java/com/twitterbot/data/SocialMedia.java
+++ src/main/java/com/twitterbot/data/SocialMedia.java
@@ -1,8 +1,12 @@
 package com.twitterbot.data;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,39 +15,41 @@ import org.jsoup.nodes.Document;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SocialMedia {
-    Twitter("Twitter followers", "title *= *\"([0-9,]+) *Followers *\"", "Something went wrong") {
+    Twitter("Twitter followers", Collections.singletonList("title *= *\"([0-9,]+) *Followers *\""), "Something went wrong") {
         @Override
         public String createAccountUrl(String account) {
             return "https://twitter.com/" + account;
         }
     },
-    Facebook("Facebook likes", "([0-9,]+) *< *span *class *= *\"_50f8 _50f4 _5kx5\" *> *likes", null) {
+    Facebook("Facebook likes", Collections.singletonList("([0-9,]+) *< *span *class *= *\"_50f8 _50f4 _5kx5\" *> *likes"), null) {
         @Override
         public String createAccountUrl(String account) {
             return "https://facebook.com/" + account;
         }
     },
-    Instagram("Instagram followers", "\"edge_followed_by\" *: *\\{ *\"count\" *: *(\\d+) *}", null) {
+    Instagram("Instagram followers", Collections.singletonList("\"edge_followed_by\" *: *\\{ *\"count\" *: *(\\d+) *}"), null) {
         @Override
         public String createAccountUrl(String account) {
             return "https://instagram.com/" + account;
         }
     },
-    YouTube("YouTube views", "<span *class *= *\"about-stat\" *>.*< *b *> *([0-9,]+) *< */b *> *views *< */span *>", null) {
+    YouTube("YouTube views", Arrays.asList("<span *class *= *\"about-stat\" *>.*< *b *> *([0-9,]+) *< */b *> *views *< */span *>", "\"viewCountText\"[\\n\\r\\s]*:[\\n\\r\\s]*\\{[\\n\\r\\s]*\"runs\"[\\n\\r\\s]*:[\\n\\r\\s]*\\[[\\n\\r\\s]*\\{[\\n\\r\\s]*\"text\"[\\n\\r\\s]*:[\\n\\r\\s]*\"([0-9,]+)\"[\\n\\r\\s]*,"), null) {
         @Override
         public String createAccountUrl(String account) {
             return "https://youtube.com/" + account + "/about";
         }
     };
+
     private static final int RETRIES = 3;
+
     @Getter
     private final String accountDescription;
-    private final Pattern countPattern;
+    private final List<Pattern> countPatterns;
     private final Pattern errorPattern;
 
-    SocialMedia(String accountDescription, String countRegex, String errorRegex) {
+    SocialMedia(String accountDescription, List<String> countRegexes, String errorRegex) {
         this.accountDescription = accountDescription;
-        this.countPattern = Pattern.compile(countRegex);
+        this.countPatterns = countRegexes.stream().map(Pattern::compile).collect(Collectors.toList());
         this.errorPattern = (errorRegex == null)
                 ? null
                 : Pattern.compile(errorRegex);
@@ -64,19 +70,28 @@ public enum SocialMedia {
                     sleep();
                 }
             } else {
-                Matcher matcher = countPattern.matcher(content);
-                if (matcher.find()) {
-                    String count = matcher.group(1);
-                    return toInt(count);
-                } else {
-                    if (hasKnownError(content)) {
-                        System.out.println("Error detected " + name() + " pattern for account " + account);
-                    } else {
-                        System.out.println("Unknown error detected " + name() + " pattern for account " + account + " for content");
-                        System.out.println(content);
-                    }
+                int count = getCountFromContent(content);
+                if (count != -1) {
+                    return count;
                 }
             }
+        }
+        return -1;
+    }
+
+    private int getCountFromContent(String content) {
+        for (Pattern countPattern : countPatterns) {
+            Matcher matcher = countPattern.matcher(content);
+            if (matcher.find()) {
+                String count = matcher.group(1);
+                return toInt(count);
+            }
+        }
+        if (hasKnownError(content)) {
+            System.out.println("Error detected " + name() + " pattern");
+        } else {
+            System.out.println("Unknown error detected " + name() + " pattern for content");
+            System.out.println(content);
         }
         return -1;
     }
